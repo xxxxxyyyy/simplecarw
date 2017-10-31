f2df29926266df321e38ac3efd566a3c9ebfa424
thinkpad <thinkpad@qq.com>
2017-10-31 22:50:28 +0800

    first simple craw

---
+++ baike_spider/spider_main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import url_manager, html_downloader, html_parser, html_outputer
+
+class SpiderMain(object):
+    def __init__(self):
+        self.urls = url_manager.UrlManager()
+        self.downloader = html_downloader.HtmlDownloader()
+        self.parser = html_parser.HtmlParser()
+        self.outputer = html_outputer.HtmlOutputer()
+
+    def craw(self, root_url):
+        count = 1
+        self.urls.add_new_url(root_url)
+        while self.urls.has_new_url():
+            #try:
+                new_url = self.urls.get_new_url()
+                print "in while"
+                print 'craw %d : %s' % (count, new_url)
+                html_cont = self.downloader.download(new_url)
+                new_urls, new_data = self.parser.parse(new_url, html_cont)
+                self.urls.add_new_urls(new_urls)
+                self.outputer.collect_data(new_data)
+                if count == 1000:
+                    break
+                count = count + 1
+            #except:
+            #    print 'craw failed'
+        self.outputer.output_html()
+
+
+if __name__=="__main__":
+    root_url = "http://baike.baidu.com/view/21087.html"
+    obj_spider = SpiderMain()
+    obj_spider.craw(root_url)
